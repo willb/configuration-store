72413fc86fd2304c334b043c72d6fe8cf5862f12
Will Benton <willb@redhat.com>
2009-08-26 08:58:24 -0500

    custom queries in FeatureArc now far more interesting (and include versioning data)

--- config-store/storemodel.rb
+++ config-store/storemodel.rb
@@ -162,15 +162,62 @@ module GridConfigStore
     declare_column :label, :integer, :not_null, references(ArcLabel)
     declare_column :deleted, :boolean, :default, :false
 
-    declare_query :arcs, "source = :source and dest = :dest and label = :label"
-    declare_query :arcs_from, "source = :source and label = :label"
-    declare_query :arcs_to, "dest = :dest and label = :label"
-    
-    # XXX:  need to select by version, only get max, also get deleted status
-    declare_custom_query :arcs_implicating, "select min(row_id), source, dest, label from __TABLE__ group by source, dest, label where (source = :feature or dest = :feature) and label = :label"
+    declare_index_on :source, :dest, :label
+    declare_index_on :source, :label
+    declare_index_on :dest, :label
+
+    declare_custom_query :arcs_q, "SELECT * FROM __TABLE__ AS fresh LEFT OUTER JOIN __TABLE__ AS fresher WHERE fresh.source = :source AND fresher.source = fresh.source AND fresh.dest = :dest AND fresher.dest = fresh.dest AND fresh.label = fresher.label AND fresh.created <= :version AND fresher.created <= :version AND fresh.created < fresher.created WHERE fresher.created IS NULL AND fresh.created <= :version"
+
+    declare_custom_query :arcs_from_q, "SELECT * FROM __TABLE__ AS fresh LEFT OUTER JOIN __TABLE__ AS fresher WHERE fresh.source = :feature AND fresher.source = fresh.source AND fresh.label = fresher.label AND fresh.created <= :version AND fresher.created <= :version AND fresh.created < fresher.created WHERE fresher.created IS NULL AND fresh.created <= :version"
+
+    declare_custom_query :arcs_to_q, "SELECT * FROM __TABLE__ AS fresh LEFT OUTER JOIN __TABLE__ AS fresher WHERE fresh.dest = :feature AND fresher.dest = fresh.dest AND fresh.label = fresher.label AND fresh.created <= :version AND fresher.created <= :version AND fresh.created < fresher.created WHERE fresher.created IS NULL AND fresh.created <= :version"
+
+    # Finds arcs to the given feature with the given label
+    # Named params include :feature, :label, and :version
+    # If version is supplied, choose the most recent revision not more
+    # recent than version.  If not, choose the most recent revision.
+    def arcs_to(args)
+      args[:version] ||= Time.now.utc.to_i
+      arcs_to_q args
+    end
+
+    # Finds arcs from the given feature with the given label
+    # Named params include :feature, :label, and :version
+    # If version is supplied, choose the most recent revision not more
+    # recent than version.  If not, choose the most recent revision.
+    def arcs_from(args)
+      args[:version] ||= Time.now.utc.to_i
+      arcs_from_q args
+    end
+
+    # Finds arcs from the given feature to the given feature, with the
+    # given label.  Named params include :source, :dest, :label, and
+    # :version If version is supplied, choose the most recent revision
+    # not more recent than version.  If not, choose the most recent
+    # revision.
+    def arcs(args)
+      args[:version] ||= Time.now.utc.to_i
+      arcs_q args
+    end
+
+    # arcs_implicating query with no versioning or deleted arcs (for reference)
+    declare_custom_query :arcs_implicating_nv, "select max(row_id), source, dest, label from __TABLE__ group by source, dest, label where (source = :feature or dest = :feature) and label = :label"
+
+    # arcs_implicating query; use the arcs_implicating method to not
+    # specify a version and get the latest.  parameters include
+    # :feature, :label, and :version; finds all arcs from or to
+    # :feature that have label :label and are the freshest version not
+    # later than :version
+    declare_custom_query :arcs_implicating_q, "SELECT * FROM __TABLE__ AS fresh LEFT OUTER JOIN __TABLE__ AS fresher WHERE fresh.source = fresher.source AND fresh.dest = fresher.dest AND fresh.label = fresher.label AND (fresh.source = :feature OR fresh.dest = :feature) AND fresh.label = :label AND fresh.created <= :version AND fresher.created <= :version AND fresh.created < fresher.created WHERE fresher.created IS NULL AND fresh.created <= :version"
+
+    def arcs_implicating(args)
+      args[:version] ||= Time.now.utc.to_i
+      arcs_implicating_q args
+    end
   end
   
-  # A relationship identifying which parameter/value pairs are implied by a given feature
+  # A relationship identifying which parameter/value pairs are implied
+  # by a given feature
   class FeatureParamMembership < Table
     alias :created :version
 
