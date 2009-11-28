63737a88154fdcd92114a534788d1e9cf8396c44
Will Benton <willb@redhat.com>
2009-11-27 19:34:26 -0600

    Added requires for QMF classes to spec file.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,14 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
+require 'mrg/grid/config/Store'
+require 'mrg/grid/config/Node'
+require 'mrg/grid/config/Configuration'
+require 'mrg/grid/config/Feature'
+require 'mrg/grid/config/Group'
+require 'mrg/grid/config/Parameter'
+require 'mrg/grid/config/Subsystem'
+
 require 'spec'
 require 'spec/autorun'
 
