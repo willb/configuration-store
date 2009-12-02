a9bc928573cf55e1cf5bd288ee38071ce3b85ccc
Will Benton <willb@redhat.com>
2009-12-02 10:18:18 -0600

    spec_helper now requires rubygems

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
+require 'rubygems'
+
 require 'mrg/grid/config/Store'
 require 'mrg/grid/config/Node'
 require 'mrg/grid/config/Configuration'
