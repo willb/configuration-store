7ba7d0d83b7aa4081ee8c2483e2d86d457dea8b1
Will Benton <willb@redhat.com>
2010-01-18 11:53:30 -0600

    mrg/grid/config.rb includes all relevant configuration-store files

---
+++ lib/mrg/grid/config.rb
@@ -0,0 +1,15 @@
+require 'spqr/spqr'
+require 'spqr/app'
+
+require 'rhubarb/rhubarb'
+
+require 'mrg/grid/config/QmfUtils'
+require 'mrg/grid/config/ArcUtils'
+require 'mrg/grid/config/ArcLabel'
+require 'mrg/grid/config/Store'
+require 'mrg/grid/config/Node'
+require 'mrg/grid/config/Configuration'
+require 'mrg/grid/config/Feature'
+require 'mrg/grid/config/Group'
+require 'mrg/grid/config/Parameter'
+require 'mrg/grid/config/Subsystem'
