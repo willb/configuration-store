1bfb4c5b4fa7acec8b1e85b52997953e6953d94f
Will Benton <willb@redhat.com>
2010-01-22 23:44:18 -0600

    Infrastructure for dump/restore, dump feature, config client lib.
    This commit adds store dumping over QMF (implemented and smoke
    tested), store dumping from within the store (implemented but
    untested), and an initial pass at configuration store convenience
    wrapper classes for Ruby consoles (used by store-dump.rb).

---
+++ bin/store-dump.rb
@@ -0,0 +1,62 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+
+require 'qmf'
+require 'mrg/grid/config-client'
+require 'mrg/grid/config-proxies'
+
+host = "localhost"
+port = 5672
+username = nil
+password = nil
+debug = :warn
+
+op = OptionParser.new do |opts|
+  opts.banner = "Usage store-dump.rb [options]"
+
+  opts.on("-h", "--host HOSTNAME", "qpid broker host (default localhost)") do |h|
+    host = h
+  end
+
+  opts.on("-p", "--port NUM", "qpid broker port (default 5672)") do |num|
+    port = num.to_i
+  end
+
+  opts.on("-U", "--user NAME", "qpid username") do |name|
+    username = name
+  end
+
+  opts.on("-P", "--password PASS", "qpid password") do |pass|
+    password = pass
+  end
+end
+
+begin
+  op.parse!
+rescue OptionParser::InvalidOption
+  puts op
+  exit
+end
+
+console = Qmf::Console.new
+
+settings = Qmf::ConnectionSettings.new
+settings.username = username if username
+settings.password = password if password
+settings.host = host
+settings.port = port
+
+connection = Qmf::Connection.new(settings)
+
+broker = console.add_connection(connection)
+
+broker.wait_for_stable
+
+store = console.object(:class=>"Store")
+
+s = Mrg::Grid::SerializedConfigs::ConfigSerializer.new(store, true, console)
+
+serialized = s.serialize
+
+puts serialized.to_yaml
--- lib/mrg/grid/config-proxies.rb
+++ lib/mrg/grid/config-proxies.rb
@@ -1,10 +1,13 @@
-require 'mrg/grid/config'
+require 'mrg/grid/config/QmfUtils'
 require 'set'
 require 'yaml'
 
+FakeList = Mrg::Grid::Config::FakeList
+FakeSet = Mrg::Grid::Config::FakeSet
+
 module Mrg
   module Grid
-    module ConfigProxies
+    module SerializedConfigs
       module DefaultStruct
         module Cm
           def saved_fields
@@ -18,7 +21,8 @@ module Mrg
         end
         
         module Im
-          def initialize(kwargs)
+          def initialize(kwargs=nil)
+            kwargs ||= {}
             sf = self.class.saved_fields
 
             sf.keys.each do |key|
@@ -45,11 +49,20 @@ module Mrg
         end
       end
       
+      class Store
+        include DefaultStruct
+        field :nodes, Set
+        field :groups, Set
+        field :params, Set
+        field :features, Set
+        field :subsystems, Set
+      end
+      
       class Feature
         include DefaultStruct
         field :name, String
         field :params, Hash
-        field :included, Set
+        field :included, Array
         field :conflicts, Set
         field :depends, Array
         field :subsystems, Set
@@ -59,8 +72,8 @@ module Mrg
         include DefaultStruct
         field :name, String
         field :is_identity_group, false
+        field :features, Array
         field :params, Hash
-        field :membership, Set
       end
       
       class Parameter
@@ -70,8 +83,10 @@ module Mrg
         field :default_val, String
         field :description, String
         field :must_change, false
-        field :level, Fixnum
-        field :needsRestart, false
+        field :level, 0
+        field :needs_restart, false
+        field :conflicts, Set
+        field :depends, Set
       end
       
       class Node
@@ -79,12 +94,137 @@ module Mrg
         field :name, String
         field :pool, String
         field :idgroup, String
+        field :membership, Array
       end        
       
       class Subsystem
         include DefaultStruct
         field :name, String
         field :params, Set
+      end
+      
+      class ConfigSerializer
+        module QmfConfigSerializer
+          # this is a no-op if we're using ConfigClients
+          def get_object(o)
+            o
+          end
+          
+          def get_instances(klass)
+            @console.objects(:class=>klass.to_s, :timeout=>45).map do |obj|
+#              obj = @console.object(:object_id=>obj)
+              ::Mrg::Grid::ConfigClient.const_get(klass).new(obj, @console)
+            end
+          end
+        end
+        
+        module InStoreConfigSerializer
+          # this is a no-op if we aren't over qmf
+          def get_object(o)
+            o
+          end
+          
+          def get_instances(klass)
+            ::Mrg::Grid::Config.const_get(klass).find_all
+          end
+        end
+        
+        def initialize(store, over_qmf=false, console=nil)
+          @store = store
+          @console = console if over_qmf
+          @struct = Store.new
+          
+          if over_qmf
+            class << self
+              include QmfConfigSerializer
+            end
+          else
+            class << self
+              include InStoreConfigSerializer
+            end
+          end
+        end
+        
+        def serialize
+          @struct.nodes = serialize_nodes
+          @struct.groups = serialize_groups
+          @struct.params = serialize_params
+          @struct.features = serialize_features
+          @struct.subsystems = serialize_subsystems
+          @struct
+        end
+        
+        private
+        def serialize_nodes
+          get_instances(:Node).map do |n|
+            node = get_object(n)
+            out = Node.new
+            out.name = node.name
+            out.pool = node.GetPool
+            # XXX:  idgroup should be set up automatically
+            out.membership = FakeList.normalize(node.GetMemberships).to_a
+            out
+          end
+        end
+        
+        def serialize_groups
+          get_instances(:Group).map do |g|
+            group = get_object(g)
+            out = Group.new
+            out.name = group.name
+            out.is_identity_group = group.is_identity_group
+            out.features = FakeList.normalize(group.GetFeatures).to_a
+            out.params = group.GetParams
+            out
+          end
+        end
+        
+        def serialize_params
+          get_instances(:Parameter).map do |p|
+            param = get_object(p)
+            out = Parameter.new
+            out.name = param.name
+            out.kind = param.GetType
+            out.default_val = param.GetDefault.to_s
+            out.description = param.GetDescription
+            out.must_change = param.GetDefaultMustChange
+            out.level = param.GetVisibilityLevel
+            out.needs_restart = param.GetRequiresRestart
+            out.conflicts = fs_normalize(param.GetConflicts)
+            out.depends = fs_normalize(param.GetDepends)
+            out
+          end
+        end
+        
+        def serialize_features
+          get_instances(:Feature).map do |f|
+            feature = get_object(f)
+            out = Feature.new
+            out.name = feature.GetName
+            out.params = feature.GetParams
+            out.included = FakeList.normalize(feature.GetFeatures).to_a
+            out.conflicts = fs_normalize(feature.GetConflicts)
+            out.depends = FakeList.normalize(feature.GetDepends).to_a
+            out.subsystems = fs_normalize(feature.GetSubsys)
+            out
+          end
+        end
+        
+        def serialize_subsystems
+          get_instances(:Subsystem).map do |s|
+            subsys = get_object(s)
+            out = Subsystem.new
+            out.name = subsys.GetName
+            out.params = fs_normalize(subsys.GetParams)
+            puts out.inspect
+            out
+          end
+        end
+        
+        def fs_normalize(fs)
+          return fs if fs.is_a? Array
+          fs.keys
+        end
       end
     end
   end  
