ff6ffa21955bb5e5cccf1e8184388002493a09c5
Will Benton <willb@redhat.com>
2010-03-02 14:46:23 -0600

    added licenses

--- bin/migrate.rb
+++ bin/migrate.rb
@@ -1,9 +1,25 @@
 #!/usr/bin/env ruby
 
+# migrate.rb
 # Performs migrations as necessary to bring a db up to date (primarily
 # intended for development, as there are some painful downsides to in-place db
 # migration).  The right approach for db migrations is to dump and load the
 # store over QMF.
+#
+# Copyright (c) 2009--2010 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 
 require 'spqr/spqr'
 require 'spqr/app'
