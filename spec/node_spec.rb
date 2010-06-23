0de039dafd1800ff0c6ab924c8c31b57982fcb7b
Will Benton <willb@redhat.com>
2010-06-23 10:55:40 -0500

    Dramatically expanded test coverage of versioned-config scenarios.

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -729,61 +729,157 @@ module Mrg
          {:before=>{PARAM(:a)=>VALUE(:a), PARAM(:b)=>VALUE(:b)}, :after=>{PARAM(:a)=>VALUE(:d), PARAM(:c)=>VALUE(:c)}, :expected_diff=>[PARAM(:a), PARAM(:b), PARAM(:c)], :description=>"when the param sets for BEFORE and AFTER are intersecting but neither is a strict superset of the other, and common params do not have common values"},
          {:before=>{PARAM(:a)=>VALUE(:a), PARAM(:b)=>VALUE(:b)}, :after=>{PARAM(:d)=>VALUE(:d), PARAM(:c)=>VALUE(:c)}, :expected_diff=>[PARAM(:a), PARAM(:b), PARAM(:c), PARAM(:d)], :description=>"when the param sets for BEFORE and AFTER are disjoint"}
         ].each do |exampleinfo|
-          [true,false].each do |dorestart|
-            
-          
-            it "should properly identify #{dorestart ? "restart" : "reconfigure"}-parameter diffs between two changed versions #{exampleinfo[:description]}" do
-              setup_whatchanged_tests
-              c_before, c_after, expected_diff = unify_param_expectations(exampleinfo[:before], exampleinfo[:after], exampleinfo[:expected_diff], dorestart)
-              node = @store.addNode("example.local.")
-              
-              node.identity_group.modifyParams("REPLACE", c_before)
-              @store.activateConfiguration
-              old_version = node.last_updated_version
-              
-              node.identity_group.modifyParams("REPLACE", c_after)
-              @store.activateConfiguration
-              new_version = node.last_updated_version
-              
-              params, restart, reconfig = node.whatChanged(old_version, new_version)
-              (params.sort - %w{WALLABY_CONFIG_VERSION}).should == expected_diff.sort
-              
-              sss = expected_diff.map {|prm| Subsystem.s_for_param(prm).map {|ss| ss.name}}.flatten.sort.uniq
-              if dorestart
-                reconfig.size.should == 0
-                restart.sort.uniq.should == sss
-              else
-                restart.size.should == 0
-                reconfig.sort.uniq.should == sss
+          [:identity_group, :default_group, :installed_feature].each do |whatchanged|
+            [true,false].each do |dorestart|
+
+              it "should properly identify #{dorestart ? "restart" : "reconfigure"}-parameter diffs between two changed versions when the #{whatchanged} changed and #{exampleinfo[:description]}" do
+                setup_whatchanged_tests
+                c_before, c_after, expected_diff = unify_param_expectations(exampleinfo[:before], exampleinfo[:after], exampleinfo[:expected_diff], dorestart)
+                node = @store.addNode("example.local.")
+                feature = @store.addFeature("Example Feature")
+                node.identity_group.modifyFeatures("ADD", [feature.name], {})
+
+                thing_to_change = case whatchanged
+                when :identity_group then node.identity_group
+                when :default_group then Group.DEFAULT_GROUP
+                when :installed_feature then feature
+                end
+
+                thing_to_change.modifyParams("REPLACE", c_before)
+                @store.activateConfiguration
+                old_version = node.last_updated_version
+
+                thing_to_change.modifyParams("REPLACE", c_after)
+                @store.activateConfiguration
+                new_version = node.last_updated_version
+
+                params, restart, reconfig = node.whatChanged(old_version, new_version)
+                (params.sort - %w{WALLABY_CONFIG_VERSION}).should == expected_diff.sort
+
+                sss = expected_diff.map {|prm| Subsystem.s_for_param(prm).map {|ss| ss.name}}.flatten.sort.uniq
+                if dorestart
+                  reconfig.size.should == 0
+                  restart.sort.uniq.should == sss
+                else
+                  restart.size.should == 0
+                  reconfig.sort.uniq.should == sss
+                end
               end
             end
-          end
 
-          it "should properly identify reconfigure-parameter diffs between two changed versions when the parameters in question have been changed from restart-parameters and #{exampleinfo[:description]}" do
-            setup_whatchanged_tests
-            c_before, c_after, expected_diff = unify_param_expectations(exampleinfo[:before], exampleinfo[:after], exampleinfo[:expected_diff], true)
-            node = @store.addNode("example.local.")
-          
-            node.identity_group.modifyParams("REPLACE", c_before)
-            @store.activateConfiguration
-            old_version = node.last_updated_version
-          
-            # change all of the parameters involved to reconfig-parameters
-            (c_before.keys | c_after.keys).each do |prm|
-              @store.getParam(prm).setRequiresRestart(false)
+            it "should properly identify reconfigure-parameter diffs between two changed versions when the #{whatchanged} changed, the parameters in question have been changed from restart-parameters, and #{exampleinfo[:description]}" do
+              setup_whatchanged_tests
+              c_before, c_after, expected_diff = unify_param_expectations(exampleinfo[:before], exampleinfo[:after], exampleinfo[:expected_diff], true)
+              node = @store.addNode("example.local.")
+              feature = @store.addFeature("Example Feature")
+              node.identity_group.modifyFeatures("ADD", [feature.name], {})
+
+              thing_to_change = case whatchanged
+              when :identity_group then node.identity_group
+              when :default_group then Group.DEFAULT_GROUP
+              when :installed_feature then feature
+              end
+
+              thing_to_change.modifyParams("REPLACE", c_before)
+              @store.activateConfiguration
+              old_version = node.last_updated_version
+
+              # change all of the parameters involved to reconfig-parameters
+              (c_before.keys | c_after.keys).each do |prm|
+                @store.getParam(prm).setRequiresRestart(false)
+              end
+
+              thing_to_change.modifyParams("REPLACE", c_after)
+              @store.activateConfiguration
+              new_version = node.last_updated_version
+
+              params, restart, reconfig = node.whatChanged(old_version, new_version)
+              (params.sort - %w{WALLABY_CONFIG_VERSION}).should == expected_diff.sort
+
+              sss = expected_diff.map {|prm| Subsystem.s_for_param(prm).map {|ss| ss.name}}.flatten.sort.uniq
+
+              restart.size.should == 0
+              reconfig.sort.uniq.should == sss
             end
           
-            node.identity_group.modifyParams("REPLACE", c_after)
-            @store.activateConfiguration
-            new_version = node.last_updated_version
+          end
           
-            params, restart, reconfig = node.whatChanged(old_version, new_version)
-            (params.sort - %w{WALLABY_CONFIG_VERSION}).should == expected_diff.sort
-          
-            sss = expected_diff.map {|prm| Subsystem.s_for_param(prm).map {|ss| ss.name}}.flatten.sort.uniq
-          
-            restart.size.should == 0
-            reconfig.sort.uniq.should == sss
+          [:identity_group, :default_group].each do |whatchanged|
+            [true,false].each do |dorestart|
+
+              it "should properly identify #{dorestart ? "restart" : "reconfigure"}-parameter diffs between two changed versions when a feature set on the #{whatchanged} changed and #{exampleinfo[:description]}" do
+                setup_whatchanged_tests
+                c_before, c_after, expected_diff = unify_param_expectations(exampleinfo[:before], exampleinfo[:after], exampleinfo[:expected_diff], dorestart)
+                node = @store.addNode("example.local.")
+                before_feature = @store.addFeature("Before Feature")
+                after_feature = @store.addFeature("After Feature")
+
+                before_feature.modifyParams("REPLACE", c_before)
+                after_feature.modifyParams("REPLACE", c_after)
+
+                thing_to_change = case whatchanged
+                when :identity_group then node.identity_group
+                when :default_group then Group.DEFAULT_GROUP
+                end
+
+                thing_to_change.modifyFeatures("REPLACE", [before_feature.name], {})
+                @store.activateConfiguration
+                old_version = node.last_updated_version
+
+                thing_to_change.modifyFeatures("REPLACE", [after_feature.name], {})
+                @store.activateConfiguration
+                new_version = node.last_updated_version
+
+                params, restart, reconfig = node.whatChanged(old_version, new_version)
+                (params.sort - %w{WALLABY_CONFIG_VERSION}).should == expected_diff.sort
+
+                sss = expected_diff.map {|prm| Subsystem.s_for_param(prm).map {|ss| ss.name}}.flatten.sort.uniq
+                if dorestart
+                  reconfig.size.should == 0
+                  restart.sort.uniq.should == sss
+                else
+                  restart.size.should == 0
+                  reconfig.sort.uniq.should == sss
+                end
+              end
+            end
+
+            it "should properly identify reconfigure-parameter diffs between two changed versions when the feature set on the #{whatchanged} changed, the parameters in question have been changed from restart-parameters, and #{exampleinfo[:description]}" do
+              setup_whatchanged_tests
+              c_before, c_after, expected_diff = unify_param_expectations(exampleinfo[:before], exampleinfo[:after], exampleinfo[:expected_diff], true)
+              node = @store.addNode("example.local.")
+              before_feature = @store.addFeature("Before Feature")
+              after_feature = @store.addFeature("After Feature")
+
+              before_feature.modifyParams("REPLACE", c_before)
+              after_feature.modifyParams("REPLACE", c_after)
+
+              thing_to_change = case whatchanged
+              when :identity_group then node.identity_group
+              when :default_group then Group.DEFAULT_GROUP
+              end
+
+              thing_to_change.modifyFeatures("REPLACE", [before_feature.name], {})
+              @store.activateConfiguration
+              old_version = node.last_updated_version
+
+              # change all of the parameters involved to reconfig-parameters
+              (c_before.keys | c_after.keys).each do |prm|
+                @store.getParam(prm).setRequiresRestart(false)
+              end
+
+              thing_to_change.modifyFeatures("REPLACE", [after_feature.name], {})
+              @store.activateConfiguration
+              new_version = node.last_updated_version
+
+              params, restart, reconfig = node.whatChanged(old_version, new_version)
+              (params.sort - %w{WALLABY_CONFIG_VERSION}).should == expected_diff.sort
+
+              sss = expected_diff.map {|prm| Subsystem.s_for_param(prm).map {|ss| ss.name}}.flatten.sort.uniq
+
+              restart.size.should == 0
+              reconfig.sort.uniq.should == sss
+            end
           end
         end
 
