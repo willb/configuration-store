77ed6648158d73646fd8acb76bd6d4c3fe5ab9e3
Will Benton <willb@redhat.com>
2010-06-23 02:27:13 -0500

    Added support for tests that rely on the configuration specified in the base db; added several such tests.

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -657,6 +657,108 @@ module Mrg
 
         end
 
+        include BaseDBFixture
+
+        def self.PARAM(x)
+          [:PARAM, x]
+        end
+
+        def self.VALUE(x)
+          [:VALUE, x]
+        end
+
+        def get_params(restart=false)
+          (self.send(restart ? :restart_params : :reconfig_params).keys - param_deps.keys - param_conflicts.keys).sort_by {rand}
+        end
+
+        def get_values
+          o = Object.new
+          def o.shift
+            @val = (@val && @val + 1) || 0
+            "value_#{@val}"
+          end
+
+          o
+        end
+
+        def self.domain(x)
+          x[0]
+        end
+
+        def setup_whatchanged_tests
+          reconstitute_db
+        end
+        
+        def unify_param_expectations(c_before, c_after, expected_diff, restart_params=false)
+          params = get_params(restart_params)
+          values = get_values
+          domains_and_keys = {:PARAM=>Hash.new {|h,k| h[k] = params.shift},
+            :VALUE=>Hash.new {|h,k| h[k] = values.shift}}
+          
+          b = {}
+          a = {}
+
+          [[b, c_before], [a, c_after]].each do |hash, source|
+            source.each do |k,v|
+              key_domain = k[0]
+              key_var = k[1]
+              val_domain = v[0]
+              val_var = v[1]
+
+              hash[domains_and_keys[key_domain][key_var]] = domains_and_keys[val_domain][val_var]
+            end
+          end
+          
+          ed = expected_diff.map {|dom,key| domains_and_keys[dom][key]}
+
+          [b,a,ed]
+        end
+          
+
+        [
+         {:before=>{}, :after=>{PARAM(:a)=>VALUE(:a)}, :expected_diff=>[PARAM(:a)], :description=>"when a param is set in AFTER but not in BEFORE"},
+         {:before=>{PARAM(:a)=>VALUE(:a)}, :after=>{}, :expected_diff=>[PARAM(:a)], :description=>"when a param is set in BEFORE but not in AFTER"},
+         {:before=>{PARAM(:a)=>VALUE(:a)}, :after=>{PARAM(:a)=>VALUE(:a)}, :expected_diff=>[], :description=>"when a param is set identically in AFTER and in BEFORE"},
+         {:before=>{PARAM(:a)=>VALUE(:a)}, :after=>{PARAM(:a)=>VALUE(:b)}, :expected_diff=>[PARAM(:a)], :description=>"when a param is set in AFTER, but to a different value than in BEFORE"},
+         {:before=>{PARAM(:a)=>VALUE(:a), PARAM(:b)=>VALUE(:b)}, :after=>{PARAM(:a)=>VALUE(:a)}, :expected_diff=>[PARAM(:b)], :description=>"when the param set for BEFORE is a strict superset of that for AFTER and common params have common values"},
+         {:before=>{PARAM(:a)=>VALUE(:a), PARAM(:b)=>VALUE(:b)}, :after=>{PARAM(:a)=>VALUE(:c)}, :expected_diff=>[PARAM(:a), PARAM(:b)], :description=>"when the param set for BEFORE is a strict superset of that for AFTER and common params do not have common values"},
+
+         {:after=>{PARAM(:a)=>VALUE(:a), PARAM(:b)=>VALUE(:b)}, :before=>{PARAM(:a)=>VALUE(:a)}, :expected_diff=>[PARAM(:b)], :description=>"when the param set for BEFORE is a strict subset of that for AFTER and common params have common values"},
+         {:after=>{PARAM(:a)=>VALUE(:a), PARAM(:b)=>VALUE(:b)}, :before=>{PARAM(:a)=>VALUE(:c)}, :expected_diff=>[PARAM(:a), PARAM(:b)], :description=>"when the param set for BEFORE is a strict subset that for AFTER and common params do not have common values"},
+         {:before=>{PARAM(:a)=>VALUE(:a), PARAM(:b)=>VALUE(:b)}, :after=>{PARAM(:a)=>VALUE(:a), PARAM(:c)=>VALUE(:c)}, :expected_diff=>[PARAM(:b), PARAM(:c)], :description=>"when the param sets for BEFORE and AFTER are intersecting but neither is a strict superset of the other, and common params have common values"},
+         {:before=>{PARAM(:a)=>VALUE(:a), PARAM(:b)=>VALUE(:b)}, :after=>{PARAM(:a)=>VALUE(:d), PARAM(:c)=>VALUE(:c)}, :expected_diff=>[PARAM(:a), PARAM(:b), PARAM(:c)], :description=>"when the param sets for BEFORE and AFTER are intersecting but neither is a strict superset of the other, and common params do not have common values"},
+         {:before=>{PARAM(:a)=>VALUE(:a), PARAM(:b)=>VALUE(:b)}, :after=>{PARAM(:d)=>VALUE(:d), PARAM(:c)=>VALUE(:c)}, :expected_diff=>[PARAM(:a), PARAM(:b), PARAM(:c), PARAM(:d)], :description=>"when the param sets for BEFORE and AFTER are disjoint"}
+        ].each do |exampleinfo|
+          [true,false].each do |dorestart|
+          
+            it "should properly identify #{dorestart ? "restart" : "reconfigure"}-parameter diffs between two changed versions #{exampleinfo[:description]}" do
+              setup_whatchanged_tests
+              c_before, c_after, expected_diff = unify_param_expectations(exampleinfo[:before], exampleinfo[:after], exampleinfo[:expected_diff], dorestart)
+              node = @store.addNode("example.local.")
+              
+              node.identity_group.modifyParams("REPLACE", c_before)
+              @store.activateConfiguration
+              old_version = node.last_updated_version
+              
+              node.identity_group.modifyParams("REPLACE", c_after)
+              @store.activateConfiguration
+              new_version = node.last_updated_version
+              
+              params, restart, reconfig = node.whatChanged(old_version, new_version)
+              (params.sort - %w{WALLABY_CONFIG_VERSION}).should == expected_diff.sort
+              
+              sss = expected_diff.map {|prm| Subsystem.s_for_param(prm).map {|ss| ss.name}}.flatten.sort.uniq
+              if dorestart
+                reconfig.size.should == 0
+                restart.sort.uniq.should == sss
+              else
+                restart.size.should == 0
+                reconfig.sort.uniq.should == sss
+              end
+            end
+          end
+        end
+
         it "should have only one identity group" do
           pending
         end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,10 @@ require 'enumerator'
 require 'spec'
 require 'spec/autorun'
 
+require 'mrg/grid/config-client'
+require 'mrg/grid/config-proxies'
+
+
 Spec::Runner.configure do |config|
   
 end
@@ -40,6 +44,48 @@ module DescribeGetterAndSetter
       
       param = @store.send(@find_msg, key)
       param.send(getmsg).should == val
+    end
+  end
+end
+
+module BaseDBFixture
+  def reconstitute_db
+    @store.storeinit("resetdb"=>"yes")
+    s = Mrg::Grid::SerializedConfigs::ConfigLoader.new(@store, dbtext)
+    s.load
+  end
+  
+  def dbtext
+    open("#{File.dirname(__FILE__)}/base-db.yaml", "r") {|db| db.read}
+  end
+
+  # defaultable parameters that need restart and their affiliated subsystems
+  def restart_params
+    @restart_params ||= Parameter.find_by(:needsRestart=>true, :must_change=>false).inject({}) do |acc,p| 
+      acc[p.name] = Subsystem.s_for_param(p).map {|ss| ss.name}
+      acc
+    end
+  end
+  
+  # as above, except no restart
+  def reconfig_params
+    @reconfig_params ||= Parameter.find_by(:needsRestart=>false, :must_change=>false).inject({}) do |acc,p| 
+      acc[p.name] = Subsystem.s_for_param(p).map {|ss| ss.name}
+      acc
+    end
+  end
+  
+  def param_deps
+    @param_deps ||= ParameterArc.find_by(:label=>ArcLabel.depends_on('param')).inject(Hash.new {|h,k| h[k] = [] ; h[k]}) do |acc,pa|
+      acc[pa.source.name] << pa.dest.name
+      acc
+    end
+  end
+  
+  def param_conflicts
+    @param_conflicts ||= ParameterArc.find_by(:label=>ArcLabel.conflicts_with('param')).inject(Hash.new {|h,k| h[k] = [] ; h[k]}) do |acc,pa|
+      acc[pa.source.name] << pa.dest.name
+      acc
     end
   end
 end
