1d3ec07e37413f730fbccc3e41331c584ae172bf
Will Benton <willb@redhat.com>
2010-07-23 15:18:46 -0500

    initial adds of proactive conflict detection specs

---
+++ spec/proactive_param_validation_spec.rb
@@ -0,0 +1,19 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+module Mrg
+  module Grid
+    module Config
+      describe Parameter do
+        before(:each) do
+          setup_rhubarb
+          @store = Store.new
+        end
+
+        after(:each) do
+          teardown_rhubarb
+        end
+        
+      end
+    end
+  end
+end
