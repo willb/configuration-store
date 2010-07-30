f571c0c4ede578bcf4f364308ffdf22ffa06f6a8
Will Benton <willb@redhat.com>
2010-07-30 17:33:14 -0500

    WIP on graph class

--- lib/mrg/grid/util/graph.rb
+++ lib/mrg/grid/util/graph.rb
@@ -1,4 +1,4 @@
-# graph.rb:  directed and undirected graphs, with (optionally-)labeled edges
+# graph.rb:  directed and undirected graphs with labeled edges
 #
 # Copyright (c) 2010 Red Hat, Inc.
 #
@@ -16,42 +16,33 @@
 
 require 'set'
 
-# XXX:  make this a labeled graph by default
-
 module Mrg
   module Grid
     module Util
       class Graph
+        attr_reader :nodes, :edges, :labels, :directed
+
         module LabeledGraph
-          def _add_edge(from, to, label="")
+          def actually_add_edge(from, to, label="")
             ensure_nodes(from, to)
             @labels << label
             edges[from] << [label, to]
           end
-
-          def labeled_edges_from(from, label)
-            edges[from]select {|l,n| l == label}.map {|l,n| n}
-          end
-        
-          def edges_from(from)
-            edges[from].map {|l,n| n}.uniq
-          end
         end
 
         module UnlabeledGraph
-          def _add_edge(from, to, label=nil)
+          def actually_add_edge(from, to, label)
             ensure_nodes(from, to)
-            edges[from] << to
+            edges[from] << ["", to]
           end
         end
-        
-        attr_reader :nodes, :edges, :labels
 
-        def initialize(directed=true, labeled_edges=false)
+        def initialize(options = nil)
+          options ||= {}
+          @labeled_edges = !options[:ignore_labels]
+          @directed = !options[:undirected]
 
-          @labeled_edges = labeled_edges
-
-          if labeled_edges
+          if @labeled_edges
             @labels = []
             class << self
               include LabeledGraph
@@ -66,25 +57,55 @@ module Mrg
           @edges = Hash.new {|h,k| h[k] = Set.new}
           @add_edge_callbacks = []
 
-          unless directed
-            @add_edge_callbacks << Proc.new {|from,to,label| _add_edge(to,from,label)}
+          unless self.directed
+            @add_edge_callbacks << Proc.new {|from,to,label| actually_add_edge(to,from,label)}
           end
         end
 
-        def edges_from(from)
-          edges[from].dup
+        def labeled_edges_from(from, *labels)
+          edges[from].select {|l,n| labels.size == 0 || labels.include?(l)}
+        end
+        
+        def edges_from(from, *labels)
+          result = labeled_edges_from(from, labels)
+
+          result.map {|l,n| n}.uniq
         end
 
         def add_edge(from, to, label="")
-          _add_edge(from, to, label)
+          actually_add_edge(from, to, label)
           @add_edge_callbacks.each do |c|
             c.call(from,to,label)
           end
         end
 
         private
+
         def ensure_nodes(*ns)
           ns.each {|n| nodes << n}
+        end
+      end
+
+      class GraphInvariantViolation < RuntimeError ; end
+
+      module TopologicalSorter
+        def self.sort(graph)
+          stack = []
+
+          post_visit_callback = Proc.new {|node, dfs| stack << node}
+          edge_callback = Proc.new do |from, to, label, dfs|
+            if dfs.discovered[to] && !dfs.processed[to]
+              raise GraphInvariantViolation.new("graph #{graph} is not a DAG (back edge from #{from} --> #{to})")
+            end
+          end
+
+          dfs = DFS.new(graph, nil, post_visit_callback, edge_callback)
+
+          graph.nodes.each do |node|
+            dfs.dfs(node, false) unless dfs.discovered[node]
+          end
+
+          stack
         end
       end
 
@@ -92,7 +113,7 @@ module Mrg
         class Done < Exception
         end
 
-        attr_reader :time
+        attr_reader :time, :entry_time, :discovered, :processed, :parent
         attr_accessor :finished
 
         def initialize(graph, pre_visit_callback=nil, post_visit_callback=nil, edge_callback=nil)
@@ -103,15 +124,13 @@ module Mrg
           @post_visit_callbacks << post_visit_callback if post_visit_callback
           @edge_callbacks ||= []
           @edge_callbacks << edge_callback if edge_callback
+          reset_self
         end
 
-        def dfs(node)
-          @time = 0
-          @discovered = {}
-          @processed = {}
-          @entry_time = {}
-          @finished = false
-          @parent = {}
+        def dfs(node, reset=true)
+          if reset
+            reset_self
+          end
           
           begin
             do_dfs(node)
@@ -119,22 +138,51 @@ module Mrg
             return
           end
         end
-        
+
         private
-        def do_dfs(node)
-          raise Done.new if finished
 
-          @discovered[node] = true
+        def reset_self
+          @time = 0
+          @discovered = {}
+          @processed = {}
+          @entry_time = {}
+          @exit_time = {}
+          @finished = false
+          @parent = {}
+        end
+
+        def tick
           @time = @time + 1
-          @entry_time[node] = time
+        end
 
-          pre_visit_callbacks.each do {|cb| cb.call(node, self)}
-
-
-
-          post_visit_callbacks.each do {|cb| cb.call(node, self)}
-
+        def do_dfs(node)
+          raise Done.new if @finished
           
+          @discovered[node] = true
+          tick
+          @entry_time[node] = time
+          
+          @pre_visit_callbacks.each {|cb| cb.call(node, self)}
+
+          outbound_edges = @graph.labeled_edges_from(node)
+          
+          outbound_edges.each do |label, to_node|
+            if !@discovered[to_node]
+              @parent[to_node] = node
+              @edge_callbacks.each {|ec| ec.call(node, to_node, label, self) }
+              do_dfs(to_node)
+            elsif @processed[to_node] || @graph.directed
+              @edge_callbacks.each {|ec| ec.call(node, to_node, label, self) }
+              raise Done.new if @finished
+            end
+          end
+
+          @post_visit_callbacks.each {|cb| cb.call(node, self)}
+
+          tick
+          @exit_time[node] = time
+
+          @processed[node] = true
         end
       end
     end
