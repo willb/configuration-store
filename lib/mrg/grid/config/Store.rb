ef1d233b7de51960b65adc5043adf09bcb613090
Will Benton <willb@redhat.com>
2010-09-01 16:33:34 -0500

    Caching parameter objects in ConfigValidating#validate.

--- lib/mrg/grid/config/Store.rb
+++ lib/mrg/grid/config/Store.rb
@@ -510,7 +510,9 @@ module Mrg
             warnings << "No node configurations have changed since the last activated config; #{validate_only ? "validate" : "activate"} request will have no effect."
           end
           
-          options = (validate_only) ? nil : {:save_for_version=>this_version}
+          options = {:param_cache=>Hash.new {|h,k| h[k] = Parameter.find_first_by_name(k)}}
+          
+          options[:save_for_version] = this_version unless validate_only
           
           results = Hash[*dirty_nodes.map {|node| node.validate(options)}.reject {|result| result == true}.flatten]
           
