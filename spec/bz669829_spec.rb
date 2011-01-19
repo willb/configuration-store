c14eeb380fef6ee8f74426785e4efa5b1edf81b2
Will Benton <willb@redhat.com>
2011-01-19 10:18:46 -0600

    bz669829_spec now runs with either snapshot first

--- spec/bz669829_spec.rb
+++ spec/bz669829_spec.rb
@@ -10,14 +10,13 @@ module Mrg
         before(:each) do
           setup_rhubarb
           @store = Store.new
-          reconstitute_db
         end
         
         after(:each) do
           teardown_rhubarb
         end
         
-        def dbtext
+        def first_dbtext
           open("#{File.dirname(__FILE__)}/bz669829_first.yaml", "r") {|db| db.read}
         end
         
@@ -27,35 +26,39 @@ module Mrg
         
         include BaseDBFixture
         
-        it "should restart the master when DAEMON_LIST changes after a snapshot load and activate" do
-          @store.activateConfiguration
-          
-          first_versions = Hash[*Node.find_all.inject([]) {|acc,n| acc << n.name; acc << n.last_updated_version}]
-          
-          reconstitute_db(second_dbtext)
-          
-          @store.activateConfiguration
-          
-          second_versions = Hash[*Node.find_all.inject([]) {|acc,n| acc << n.name; acc << n.last_updated_version}]
-          
-          Node.find_all.each do |n|
-            fv = first_versions[n.name]
-            sv = second_versions[n.name]
-            
-            # if the configuration changed at all for this node
-            if fv && sv
-              old_config = n.getConfig("version"=>fv)
-              new_config = n.getConfig("version"=>sv)
-              
-              if old_config["DAEMON_LIST"] != new_config["DAEMON_LIST"]
-                params, restart, reconfig = n.whatChanged(fv,sv)
-                params.should include("DAEMON_LIST")
-                restart.should include("master")
-              end
+        {"first->second"=>[:first_dbtext, :second_dbtext], "second->first"=>[:second_dbtext,:first_dbtext]}.each do |order,msgs|
+        
+          it "should restart the master when DAEMON_LIST changes after a snapshot load and activate (#{order})" do
+            reconstitute_db(self.send(msgs[0]))
 
+            @store.activateConfiguration
+
+            first_versions = Hash[*Node.find_all.inject([]) {|acc,n| acc << n.name; acc << n.last_updated_version}]
+
+            reconstitute_db(self.send(msgs[1]))
+
+            @store.activateConfiguration
+
+            second_versions = Hash[*Node.find_all.inject([]) {|acc,n| acc << n.name; acc << n.last_updated_version}]
+
+            Node.find_all.each do |n|
+              fv = first_versions[n.name]
+              sv = second_versions[n.name]
+
+              # if the configuration changed at all for this node
+              if fv && sv
+                old_config = n.getConfig("version"=>fv)
+                new_config = n.getConfig("version"=>sv)
+
+                if old_config["DAEMON_LIST"] != new_config["DAEMON_LIST"]
+                  params, restart, reconfig = n.whatChanged(fv,sv)
+                  params.should include("DAEMON_LIST")
+                  restart.should include("master")
+                end
+
+              end
             end
           end
-          
         end
       end
     end
