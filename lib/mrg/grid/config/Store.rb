b7596338719c769da62dbb9c29bc206287b2fb1e
Will Benton <willb@redhat.com>
2012-05-30 17:20:04 -0500

    Creating a new node now activates it (and only it).
    ===BZ=== 748507 -- candidate fix

--- lib/mrg/grid/config/Store.rb
+++ lib/mrg/grid/config/Store.rb
@@ -370,7 +370,12 @@ module Mrg
           n.modifyMemberships("ADD", [Group::SKELETON_GROUP_NAME], {}) if Store::ENABLE_SKELETON_GROUP
 
           # XXX:  this is almost certainly not the right place to do this (copying default config over for newly-created nodes)
-          n.last_updated_version = seek_version || ConfigVersion.dupVersionedNodeConfig(Group::DEFAULT_GROUP_NAME, name)
+          if seek_version
+            n.last_updated_version = seek_version
+          else
+            validate_and_activate(false, [n])
+          end
+
           n
         end
         
