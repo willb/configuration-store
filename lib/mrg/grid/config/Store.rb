528c124ac718cec262df21e0d8962c2e84f72d49
Will Benton <willb@redhat.com>
2012-06-20 23:18:36 -0500

    fixes typo in 1d1c2cf9

--- lib/mrg/grid/config/Store.rb
+++ lib/mrg/grid/config/Store.rb
@@ -716,7 +716,7 @@ module Mrg
           # we're activating this configuration; save the default group configuration by itself 
           # if possible and if we aren't dealing with an explicit node list
           unless explicit_nodelist
-            validate_and_activate(false, [Group.DEFAULT_GROUP], this_version) unless ConfigVersion.hasVersionedNodeConfig(Group.DEFAULT_GROUP_NAME, this_version)
+            validate_and_activate(false, [Group.DEFAULT_GROUP], this_version) unless ConfigVersion.hasVersionedNodeConfig(Group::DEFAULT_GROUP_NAME, this_version)
           end
           DirtyElement.delete_all
           
